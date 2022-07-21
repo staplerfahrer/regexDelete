219b1b85fa73375d0f68e360017720f626a2a6fc
Jacob Bruinsma <jacob.a.bruinsma@gmail.com>
2022-07-21 15:33:03 -0500

    created a main(), added better status printing

--- regexDelete.py
+++ regexDelete.py
@@ -1,34 +1,51 @@
 import glob, re, shutil, sys, os, os.path
 
-if len(sys.argv) < 3:
-	print('Usage:\nregexDelete -t "test1.*\.jpg$"\nregexDelete -d "pattern1.*\.jpg$" "pattern2.*\.bmp$"')
-	sys.exit()
+def main(testing, arguments):
+	print(arguments)
 
-testing = not (sys.argv[1] == '-d')
-arguments = sys.argv[2:]
-
-print(arguments)
-
-if not testing:
-	print('Delete mode.\n')
-else:
-	print('Test mode.\n')
-
-patterns = [re.compile(argument, flags=re.IGNORECASE) for argument in arguments]
-
-allFiles = glob.glob("**/*", recursive=True)
-
-matchingPaths = [file for file in allFiles if any([pattern.search(file) for pattern in patterns])]
-
-for path in matchingPaths:
-	print(path)
 	if not testing:
-		if os.path.isfile(path):
-			os.remove(path)
+		print('Delete mode.\n')
+	else:
+		print('Test mode.\n')
+
+	patterns = [re.compile(argument, flags=re.IGNORECASE) for argument in arguments]
+
+	allFiles = glob.glob("**/*", recursive=True)
+	print(f'{os.getcwd()} contains {len(allFiles)} files and/or directories.')
+	matchingPaths = [file for file in allFiles if any([pattern.search(file) for pattern in patterns])]
+
+	for path in matchingPaths:
+		print(path+' ', end='')
+		if not testing:
+			if os.path.isfile(path):
+				try:
+					os.remove(path)
+					print('deleted.')
+				except FileNotFoundError as fe:
+					pass
+				except Exception as e:
+					print('NOT deleted. Error:')
+					print(e)
+			else:
+				try:
+					shutil.rmtree(path)
+					print('deleted.')
+				except FileNotFoundError as fe:
+					pass
+				except Exception as e:
+					print('NOT deleted. Error:')
+					print(e)
 		else:
-			try:
-				shutil.rmtree(path)
-			except FileNotFoundError as fe:
-				pass
-			except Exception as e:
-				print(e)
+			print('matched.')
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print('Usage:\n'
+				'regexDelete -t "test1.*\.jpg$"\n'
+				'regexDelete -d "pattern1.*\.jpg$" "pattern2.*\.bmp$"')
+		sys.exit()
+
+	testing = not (sys.argv[1] == '-d')
+	arguments = sys.argv[2:]
+
+	main(testing, arguments)
