bebdf67616782e0f9e24b70fa4b53d9309dcd6dd
Jacob Bruinsma <jacob.a.bruinsma@gmail.com>
2022-07-21 15:33:33 -0500

    line wrapped

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['regexDelete.py'], options={'py2exe': {'bundle_files': 1, 'compressed': True}}, zipfile=None)
+setup(console=['regexDelete.py'], 
+		options={'py2exe': {'bundle_files': 1, 'compressed': True}}, 
+		zipfile=None)
